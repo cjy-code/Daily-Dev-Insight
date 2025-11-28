0a536d582bd18b7f45c65506b876d7f60391105c
DESKTOP-6IC0TBR\user <wls1514@diquest.com>
2025-11-28 12:25:16 +0900

    Project Init: Backend (Spring Boot, MyBatis, Oracle) & Docker Compose

---
+++ backend/src/main/java/com/dailydevinsight/DailyDevInsightApplication.java
@@ -0,0 +1,15 @@
+package com.dailydevinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class DailyDevInsightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyDevInsightApplication.class, args);
+    }
+
+}
